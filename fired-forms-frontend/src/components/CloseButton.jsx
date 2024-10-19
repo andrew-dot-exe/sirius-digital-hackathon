6047a587d8da68a243a1a24e3c2abf904c5e487f
Danil Kupcov <danilkup23@yandex.ru>
2024-10-19 07:35:07 +0400

    update

--- fired-forms-frontend/src/components/CloseButton.jsx
+++ fired-forms-frontend/src/components/CloseButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../styles/СloseButton.css';
 
 const CloseButton = ({onClick}) =>
-{
+{   
     return (
         <button  className="close-button" onClick={onClick}>
             Закрыть
